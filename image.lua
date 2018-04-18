60e27fd09163420093584bb4f11481fa07495157
Comcx <comcx@outlook.com>
2018-04-18 19:15:38 +0800

    Update image.lua

--- image.lua
+++ image.lua
@@ -10,7 +10,7 @@ drawBGImage = function(image, conf_window, x, y)
 	local x, y = x or 0, y or 0
 
 	love.graphics.setColor(color_keep)
-	quad_bg	= love.graphics.newQuad(0, 0, conf_window.width, conf_window.height,
+	local quad_bg	= love.graphics.newQuad(0, 0, conf_window.width, conf_window.height,
 									image:getWidth(), image:getHeight())
 
 	love.graphics.draw(image, quad_bg, x, y)
